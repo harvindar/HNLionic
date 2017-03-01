a3faf2d81d44449c4b3661bde2d613d089ecbbe7
Dnyaneshwar Tukaram Kubal <dnyaneshwar.kubal@infogain.com>
2017-03-01 16:53:08 +0530

    Dashboard Changes

--- src/pages/dashboard/dashboard.ts
+++ src/pages/dashboard/dashboard.ts
@@ -20,12 +20,18 @@ export class DashboardPage {
   }
 
   ////////////
-  public lineChartData:Array<any> = [
-        [0, 100, 0, 0, 0, 203, 0]
-  
+  public lineChartData: any[] = [
+    { data: [130, 0, 0, 0, 0, 203, 0], label: 'Glucose' },
+    { data: [120, 0, 0, 0, 0, 125, 0], label: 'Calcium' },
+        { data: [130, 0, 0, 0, 0, 120, 0], label: 'Albumin' }
+
   ];
+
   public lineChartLabels:Array<any> = ['Mon', 'Tue','Wed', 'Thu','Fri', 'Sat','Sun'];
+
+
   public lineChartType:string = 'bar';
+  public series: Array<any> = ['Se1','Se2','Se3'];
   public graphtype : string = 'Weekly';
 
   private lineChartOptions = {
@@ -36,25 +42,78 @@ export class DashboardPage {
         }
       }]
     },
-    legend : {
- display : false,
-
-
-    }
+responsive: true
   };
 
  public MonthlyChartData:Array<any> = [
-     [100, 180, 170, 130] 
-    
+ { data: [130, 0, 100, 0], label: 'Glucose' },
+    { data: [120, 0, 120, 0], label: 'Calcium' },
+        { data: [100, 0, 103, 0], label: 'Albumin' }    
   ];
   public MonthlyLabels:Array<any> = ['Week1', 'Week2', 'Week3', 'Week4'];
   public MonthlyChartType:string = 'line';
 
  public AllChartData:Array<any> = [
-     [145, 165, 170, 155] ,[145, 165, 170, 155] ,[145, 165, 170, 155] ,[145, 165, 170, 155] 
+    { data: [100, 200, 230, 250], label: 'Glucose' },
+    { data: [120, 120, 120, 100], label: 'Calcium' },
+        { data: [100, 100, 130, 100], label: 'Albumin' }  
     
   ];
-  public AllLabels:Array<any> = ['Jan', 'Feb', 'Mar', 'April'];
+  public AllLabels:Array<any> = ['Nov', 'Dec', 'Jan', 'Feb'];
   public AlllChartType:string = 'line';
+ 
+setgraph (grapselcted) {
+  console.log(this.graphtype);
+
+switch(grapselcted) 
+{
+case "line" : 
+        if(this.graphtype == "Weekly" ) 
+        {
+          this.lineChartType = "line";
+        }
+          if(this.graphtype == "Monthly" ) 
+        {
+          this.MonthlyChartType = "line";
+        }
+          if(this.graphtype == "All" ) 
+        {
+          this.AlllChartType = "line";
+        }
+break;
+
+case "bar" : 
+   if(this.graphtype == "Weekly" ) 
+           {
+          this.lineChartType = "bar";
+        }
+          if(this.graphtype == "Monthly" ) 
+        {
+          this.MonthlyChartType = "bar";
+        }
+          if(this.graphtype == "All" ) 
+        {
+          this.AlllChartType = "bar";
+        }
+        
+break;
+
+case "doughnut" :
+if(this.graphtype == "Weekly" ) 
+           {
+          this.lineChartType = "doughnut";
+        }
+          if(this.graphtype == "Monthly" ) 
+        {
+          this.MonthlyChartType = "doughnut";
+        }
+          if(this.graphtype == "All" ) 
+        {
+          this.AlllChartType = "doughnut";
+        }
+break;
+}
+
+}
 
 }
